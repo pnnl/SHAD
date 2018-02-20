869455ec4fc0ae1bcb8f99872d5b007607c2908a
Marco Minutoli <marco.minutoli@pnnl.gov>
2018-02-20 11:27:41 -0800

    [pnnl/SHAD#13] Remove test for -pthread compiler flag.

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -36,10 +36,6 @@ set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -g")
 set(CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS_RELEASE} ${CMAKE_C_FLAGS_DEBUG}")
 
 # CXX compiler flags:
-check_cxx_compiler_flag("-pthread" CXX_SUPPORT_PTHREAD_FLAG)
-if (CXX_SUPPORT_PTHREAD_FLAG)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread")
-endif()
 
 ## CXX Release Build flags:
 set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3")
