a41fae15ddc689bb157fe8f6a9f46385e6e983da
Minutoli, Marco <marco.minutoli@pnnl.gov>
2018-07-20 15:32:47 -0700

    [pnnl/SHAD#51] Mark locality constructor as constexpr.

--- include/shad/runtime/locality.h
+++ include/shad/runtime/locality.h
@@ -64,7 +64,7 @@ class Locality {
   /// Initialize the newly created object with a specific locality ID.
   ///
   /// @param id The locality ID to be assigned.
-  explicit Locality(const uint32_t id) : id_(id) {}
+  explicit constexpr Locality(const uint32_t id) : id_(id) {}
 
   /// @brief Copy-Constructor.
   Locality(const Locality& rhs) = default;
