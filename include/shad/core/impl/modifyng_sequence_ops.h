aa5b804b6da3838540c6175e01ae34565538e086
Maurizio Drocco <maurizio.drocco@pnnl.gov>
2018-09-27 14:15:42 -0700

    [pnnl/SHAD#76] removed useless variable in shad::impl::generate

--- include/shad/core/impl/modifyng_sequence_ops.h
+++ include/shad/core/impl/modifyng_sequence_ops.h
@@ -170,7 +170,7 @@ void generate(distributed_parallel_tag&& policy, ForwardIt first,
           // call the generator to align with the offset
           auto it = itr_traits::iterator_from_local(begin, end, lbegin);
           for (auto calls = std::distance(begin, it); calls; --calls)
-            auto tmp = generator();
+            generator();
 
           std::generate(lbegin, lend, generator);
         },
@@ -202,7 +202,7 @@ void generate(distributed_sequential_tag&& policy, ForwardIt first,
                     auto it =
                         itr_traits::iterator_from_local(begin, end, lbegin);
                     for (auto calls = std::distance(begin, it); calls; --calls)
-                      auto tmp = generator();
+                      generator();
 
                     std::generate(lbegin, lend, generator);
                   },
