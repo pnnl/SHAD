a553fbced8a034b65201e62726bd18deb9d22941
Maurizio Drocco <maurizio.drocco@pnnl.gov>
2018-10-19 15:43:11 -0700

    [pnnl/SHAD#144] co-localizing shad::copy

--- include/shad/core/impl/modifyng_sequence_ops.h
+++ include/shad/core/impl/modifyng_sequence_ops.h
@@ -38,6 +38,174 @@
 namespace shad {
 namespace impl {
 
+namespace copy_impl {
+template <class ForwardIt1, class ForwardIt2>
+ForwardIt2 fw_kernel(std::forward_iterator_tag, ForwardIt1 begin1,
+                     ForwardIt1 end1, ForwardIt2 d_begin) {
+  using itr_traits1 = distributed_iterator_traits<ForwardIt1>;
+  using itr_traits2 = distributed_iterator_traits<ForwardIt2>;
+
+  // lfirst1, llast1: local range for the input-range portion
+  auto local_range = itr_traits1::local_range(begin1, end1);
+  auto lfirst1 = local_range.begin();
+  auto llast1 = local_range.end();
+
+  // d_first, d_last: output-range portion corresponding to the
+  // input-range portion
+  auto first1 = itr_traits1::iterator_from_local(begin1, end1, lfirst1);
+  auto last1 = itr_traits1::iterator_from_local(begin1, end1, llast1);
+  auto d_first = d_begin;
+  std::advance(d_first, std::distance(begin1, first1));
+  auto d_last = d_first;
+  std::advance(d_last, std::distance(lfirst1, llast1));
+
+  // loc_d_lfirst, loc_d_llast: local output-range portion
+  auto loc_d_range = itr_traits2::local_range(d_first, d_last);
+  auto loc_d_lfirst = loc_d_range.begin();
+  auto loc_d_llast = loc_d_range.end();
+
+  // loc_d_first, loc_d_last: global range for the local output-range
+  // portion
+  auto loc_d_first =
+      itr_traits2::iterator_from_local(d_first, d_last, loc_d_lfirst);
+  auto loc_d_last =
+      itr_traits2::iterator_from_local(d_first, d_last, loc_d_llast);
+
+  // process portions
+  auto in_it1 = lfirst1, in_it2 = lfirst1;
+  auto prefix_len = std::distance(d_first, loc_d_first);
+  std::advance(in_it2, prefix_len);
+  std::copy(in_it1, in_it2, d_first);
+
+  auto local_len = std::distance(loc_d_first, loc_d_last);
+  in_it1 = in_it2;
+  std::advance(in_it2, local_len);
+  std::copy(in_it1, in_it2, loc_d_lfirst);
+
+  auto suffix_len = std::distance(loc_d_last, d_last);
+  in_it1 = in_it2;
+  std::advance(in_it2, suffix_len);
+  return std::copy(in_it1, in_it2, loc_d_last);
+}
+
+// distributed_sequential, output_iterator_tag
+template <class ForwardIt1, class ForwardIt2>
+ForwardIt2 copy(std::output_iterator_tag, distributed_sequential_tag&& policy,
+                ForwardIt1 first1, ForwardIt1 last1, ForwardIt2 d_first) {
+  using itr_traits = distributed_iterator_traits<ForwardIt1>;
+  auto localities = itr_traits::localities(first1, last1);
+  ForwardIt2 res = d_first;
+  for (auto locality = localities.begin(), end = localities.end();
+       locality != end; ++locality) {
+    rt::executeAtWithRet(
+        locality,
+        [](const std::tuple<ForwardIt1, ForwardIt1, ForwardIt2>& args,
+           ForwardIt2* res_ptr) {
+          auto d_first_ = std::get<2>(args);
+          auto local_range =
+              itr_traits::local_range(std::get<0>(args), std::get<1>(args));
+          auto begin = local_range.begin();
+          auto end = local_range.end();
+          *res_ptr = std::copy(begin, end, d_first_);
+        },
+        std::make_tuple(first1, last1, res), &res);
+  }
+  return res;
+}
+
+// distributed_sequential, forward_iterator_tag
+template <class ForwardIt1, class ForwardIt2>
+ForwardIt2 copy(std::forward_iterator_tag, distributed_sequential_tag&& policy,
+                ForwardIt1 begin1, ForwardIt1 end1, ForwardIt2 d_begin) {
+  using itr_traits = distributed_iterator_traits<ForwardIt1>;
+  auto localities = itr_traits::localities(begin1, end1);
+  auto res = d_begin;
+  for (auto locality = localities.begin(), end = localities.end();
+       locality != end; ++locality) {
+    rt::executeAtWithRet(
+        locality,
+        [](const std::tuple<ForwardIt1, ForwardIt1, ForwardIt2>& args,
+           ForwardIt2* res_ptr) {
+          *res_ptr = fw_kernel(std::forward_iterator_tag{}, std::get<0>(args),
+                               std::get<1>(args), std::get<2>(args));
+        },
+        std::make_tuple(begin1, end1, d_begin), &res);
+  }
+  return res;
+}
+
+// distributed_parallel, output_iterator_tag
+template <class ForwardIt, class OutputIt>
+OutputIt copy(std::output_iterator_tag, distributed_parallel_tag&& policy,
+              ForwardIt first1, ForwardIt last1, OutputIt d_first) {
+  using itr_traits = distributed_iterator_traits<ForwardIt>;
+  auto localities = itr_traits::localities(first1, last1);
+  std::vector<OutputIt> res(localities.size(), d_first);
+  auto res_it = res.begin();
+  rt::Handle h;
+  for (auto locality = localities.begin(), end = localities.end();
+       locality != end; ++locality, ++res_it) {
+    rt::asyncExecuteAtWithRet(
+        h, locality,
+        [](rt::Handle&, const std::tuple<ForwardIt, ForwardIt, OutputIt>& args,
+           OutputIt* res_ptr) {
+          auto gbegin = std::get<0>(args);
+          auto gend = std::get<1>(args);
+          auto local_range = itr_traits::local_range(gbegin, gend);
+          auto begin = local_range.begin();
+          auto end = local_range.end();
+          auto it = itr_traits::iterator_from_local(gbegin, gend, begin);
+          auto d_first_ = std::get<2>(args);
+          *res_ptr = std::copy(begin, end, d_first_);
+        },
+        std::make_tuple(first1, last1, d_first), &(*res_it));
+  }
+  rt::waitForCompletion(h);
+  return res.back();
+}
+
+// distributed_parallel, forward_iterator_tag
+template <class ForwardIt1, class ForwardIt2>
+ForwardIt2 copy(std::forward_iterator_tag, distributed_parallel_tag&& policy,
+                ForwardIt1 begin1, ForwardIt1 end1, ForwardIt2 d_begin) {
+  using itr_traits = distributed_iterator_traits<ForwardIt1>;
+  auto localities = itr_traits::localities(begin1, end1);
+  std::vector<ForwardIt2> res(localities.size(), d_begin);
+  auto res_it = res.begin();
+  rt::Handle h;
+  for (auto locality = localities.begin(), end = localities.end();
+       locality != end; ++locality, ++res_it) {
+    rt::asyncExecuteAtWithRet(
+        h, locality,
+        [](rt::Handle&,
+           const std::tuple<ForwardIt1, ForwardIt1, ForwardIt2>& args,
+           ForwardIt2* res_ptr) {
+          *res_ptr = fw_kernel(std::forward_iterator_tag{}, std::get<0>(args),
+                               std::get<1>(args), std::get<2>(args));
+        },
+        std::make_tuple(begin1, end1, d_begin), &(*res_it));
+  }
+  rt::waitForCompletion(h);
+  return res.back();
+}
+}  // namespace copy_impl
+
+template <class ForwardIt1, class OutputIt>
+OutputIt copy(distributed_sequential_tag&& policy, ForwardIt1 begin1,
+              ForwardIt1 end1, OutputIt d_begin) {
+  return copy_impl::copy(
+      typename std::iterator_traits<OutputIt>::iterator_category{},
+      std::forward<distributed_sequential_tag>(policy), begin1, end1, d_begin);
+}
+
+template <class ForwardIt1, class OutputIt>
+OutputIt copy(distributed_parallel_tag&& policy, ForwardIt1 begin1,
+              ForwardIt1 end1, OutputIt d_begin) {
+  return copy_impl::copy(
+      typename std::iterator_traits<OutputIt>::iterator_category{},
+      std::forward<distributed_parallel_tag>(policy), begin1, end1, d_begin);
+}
+
 template <typename ForwardIt, typename T>
 void fill(distributed_parallel_tag&& policy, ForwardIt first, ForwardIt last,
           const T& value) {
