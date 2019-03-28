b07838457414c5d0e791466d497f5c85a65b9e74
Marco Minutoli <marco.minutoli@pnnl.gov>
2019-03-28 14:43:44 -0700

    [#164] First basic implementation of Pi estimation.

---
+++ examples/pi/pi.cc
@@ -0,0 +1,72 @@
+//===------------------------------------------------------------*- C++ -*-===//
+//
+//                                     SHAD
+//
+//      The Scalable High-performance Algorithms and Data Structure Library
+//
+//===----------------------------------------------------------------------===//
+//
+// Copyright 2018 Battelle Memorial Institute
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy
+// of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+//
+//===----------------------------------------------------------------------===/
+
+#include <iostream>
+#include <iomanip>
+#include <random>
+
+#include "shad/core/array.h"
+#include "shad/core/algorithm.h"
+#include "shad/core/numeric.h"
+#include "shad/core/execution.h"
+#include "shad/data_structures/one_per_locality.h"
+
+namespace shad {
+
+int main(int argc, char *argv[]) {
+
+  shad::array<size_t, 128> counters;
+
+  size_t numberOfPoints = 1e12;
+  size_t numberOfPointsPerSim = numberOfPoints / counters.size();
+
+  shad::generate(
+      shad::distributed_parallel_tag{},
+      counters.begin(), counters.end(),
+      [=]() -> size_t {
+        size_t counter = 0;
+        std::random_device rd;
+        std::default_random_engine G(rd());
+        std::uniform_real_distribution<double> dist(0.0, 1.0);
+        for (size_t i = 0; i < numberOfPointsPerSim; ++i) {
+          double x = dist(G);
+          double y = dist(G);
+          if ((x * x + y * y) < 1) {
+            ++counter;
+          }
+        }
+        return counter;
+      });
+
+  size_t count = shad::reduce(
+      shad::distributed_sequential_tag{}, counters.begin(), counters.end());
+
+  std::cout << "Pi is roughly "
+            << std::setprecision(20)
+            << (4.0 * count) / numberOfPoints << std::endl;
+
+  return 0;
+}
+
+}
