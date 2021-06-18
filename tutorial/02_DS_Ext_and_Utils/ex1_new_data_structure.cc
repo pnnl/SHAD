1ffbce92fd5b3f6e4b8f26ba20efb29c4684af6d
vito <vitogiovanni.castellana@pnnl.gov>
2021-06-17 17:41:23 -0700

    added scatter example

--- tutorial/02_DS_Ext_and_Utils/ex1_new_data_structure.cc
+++ tutorial/02_DS_Ext_and_Utils/ex1_new_data_structure.cc
@@ -39,7 +39,29 @@ int main(int argc, char **argv) {
         }
       },
       anInt->GetGlobalID());
-   
+  
+  std::cout << "\nLet's try the scatter operation\n" << std::endl;
+
+  std::vector<int> localData(rt::numLocalities());
+  for (size_t i = 0; i < localData.size(); ++i) {
+    localData[i] = i;
+  }
+  
+  anInt->scatter(localData);
+
+std::cout << "\nLet's check all the values again\n" << std::endl;
+
+  rt::executeOnAll(
+      [](const shad::MyNewDS<int>::ObjectID& oid) {
+        auto ptr = shad::MyNewDS<int>::GetPtr(oid);
+        if(ptr != nullptr) {
+          std::cout << "Pointer looks good on "
+                    << rt::thisLocality() << ", value is "
+                    << static_cast<int>(*ptr) << std::endl;
+        }
+      },
+      anInt->GetGlobalID());
+
   shad::MyNewDS<int>::Destroy(anInt->GetGlobalID());
 
 
