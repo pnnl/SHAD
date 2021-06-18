ef6bbf20a7d7fbc0a03b7014f6891c86ee99925f
vito <vitogiovanni.castellana@pnnl.gov>
2021-06-17 17:00:31 -0700

    update on new ds example

--- tutorial/02_DS_Ext_and_Utils/ex1_new_data_structure.cc
+++ tutorial/02_DS_Ext_and_Utils/ex1_new_data_structure.cc
@@ -18,7 +18,28 @@ int main(int argc, char **argv) {
         }
       },
       anInt->GetGlobalID());
+  std::cout << "\nLet's update the values\n" << std::endl;
 
+  rt::executeOnAll(
+      [](const shad::MyNewDS<int>::ObjectID& oid) {
+        auto ptr = shad::MyNewDS<int>::GetPtr(oid);
+        (*ptr) = 10 + static_cast<uint32_t>(rt::thisLocality());
+      },
+    anInt->GetGlobalID());
+
+  std::cout << "\nLet's check all the values are correct\n" << std::endl;
+
+  rt::executeOnAll(
+      [](const shad::MyNewDS<int>::ObjectID& oid) {
+        auto ptr = shad::MyNewDS<int>::GetPtr(oid);
+        if(ptr != nullptr) {
+          std::cout << "Pointer looks good on "
+                    << rt::thisLocality() << ", value is "
+                    << static_cast<int>(*ptr) << std::endl;
+        }
+      },
+      anInt->GetGlobalID());
+   
   shad::MyNewDS<int>::Destroy(anInt->GetGlobalID());
 
 
