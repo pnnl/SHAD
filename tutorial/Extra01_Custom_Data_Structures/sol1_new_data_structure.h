33b0abbf1e7546106fc752e0c341db84fd6b2603
Vito G Castellana <vitogiovanni.castellana@pnnl.gov>
2025-05-30 17:26:27 -0700

    added extra examples

---
+++ tutorial/Extra01_Custom_Data_Structures/sol1_new_data_structure.h
@@ -0,0 +1,113 @@
+#ifndef INCLUDE_SHAD_DATA_STRUCTURES_MY_NEWEST_DS_H_
+#define INCLUDE_SHAD_DATA_STRUCTURES_MY_NEWEST_DS_H_
+
+#include "shad/data_structures/abstract_data_structure.h"
+
+namespace shad {
+
+/// @brief Wrapper that instantiate one object per Locality in the system.
+///
+/// This Wrapper triggers the instantiation of one object per Locality in the
+/// system.
+///
+/// @warning Writes are not propagated across the system.
+///
+/// @tparam T The typen of the objects that will be instantiated.
+template <typename T>
+class MyNewDS : public AbstractDataStructure<MyNewDS<T>> {
+ public:
+  using ObjectID = typename AbstractDataStructure<MyNewDS<T>>::ObjectID;
+  using SharedPtr =
+      typename AbstractDataStructure<MyNewDS<T>>::SharedPtr;
+
+  /// @brief Create method.
+  ///
+  /// Creates instances of a T object on each locality.
+  ///
+  /// @tparam Args The list of types needed to build an instance of type T
+  ///
+  /// @param args The parameter pack to be forwarded to T' constructor.
+  /// @return A shared pointer to the local MyNewDS instance.
+#ifdef DOXYGEN_IS_RUNNING
+  template <typename... Args>
+  static SharedPtr Create(Args... args);
+#endif
+
+  /// @brief Retieve the Global Identifier.
+  ///
+  /// @return The global identifier associated with the array instance.
+  ObjectID GetGlobalID() const { return oid_; }
+
+  /// @brief Access the local instance.
+  ///
+  /// @return A pointer to the local instance.
+  T* const operator->() { return &localInstance_; }
+
+  /// @brief Assign the an instance of T to the local object.
+  ///
+  /// @tparam T the type of the allocated.
+  /// @param rhs The right-hand side of the assignment.
+  MyNewDS<T>& operator=(const T& rhs) {
+    localInstance_ = rhs;
+    return *this;
+  }
+
+  /// @brief Retrieve a copy of the local instance.
+  explicit operator T() const { return localInstance_; }
+
+  /// @brief Scatter operation
+  void scatter(std::vector<T> &v) {
+    uint32_t idx = 0;
+    rt::Handle h;
+    for (auto loc : shad::rt::allLocalities()) {
+      std::pair<ObjectID, T> args(oid_, v[idx]);
+      rt::asyncExecuteAt(h, loc,
+        [](rt::Handle&, const std::pair<ObjectID, T> &args) {
+          auto ptr = MyNewDS<T>::GetPtr(args.first);
+          (*ptr) = args.second;
+        },  
+        args);
+      ++idx;
+    }
+    rt::waitForCompletion(h);
+  }
+
+
+  // Exercise
+  // Implement the gather operation
+  // Hint1: Very similar to the scatter
+  // Hint2: We have seen the needed shad::rt function in ex2_rdma
+  std::vector<T> gather() {
+    std::vector<T> res(rt::numLocalities());
+    uint32_t idx = 0;
+    rt::Handle h;
+    for (auto loc : shad::rt::allLocalities()) {
+      rt::asyncExecuteAtWithRet(h, loc,
+        [](rt::Handle&, const ObjectID &oid, T* res) {
+          auto ptr = MyNewDS<T>::GetPtr(oid);
+          *res = static_cast<T>(*ptr);
+        },  
+        oid_, &res[idx]);
+      ++idx;
+    }
+    rt::waitForCompletion(h);
+    return res;
+  }
+
+ protected:
+  /// @brief Constructor.
+  template <typename... Args>
+  explicit MyNewDS(ObjectID oid, Args... args)
+      : oid_{oid}, localInstance_{args...} {}
+
+ private:
+  template <typename>
+  friend class AbstractDataStructure;
+
+  ObjectID oid_;
+  T localInstance_;
+};
+
+}  // namespace shad
+
+#endif  // INCLUDE_SHAD_DATA_STRUCTURES_MY_NEWEST_DS_H_
---
+++ tutorial/Extra02_SHAD_Extensions/utils_and_ext.cc
@@ -0,0 +1,81 @@
+#include <atomic>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <utility>
+
+#include "shad/data_structures/set.h"
+#include "shad/extensions/data_types/data_types.h"
+#include "shad/extensions/graph_library/edge_index.h"
+#include "shad/runtime/runtime.h"
+
+namespace shad {
+
+constexpr size_t n_records = 100;
+constexpr size_t n_cols = 4;
+using entry_t = std::array<uint64_t, n_cols>;
+static const shad::data_types::schema_t entry_schema = {
+  std::make_pair("domain", shad::data_types::UINT),
+  std::make_pair("server", shad::data_types::IP_ADDRESS),
+  std::make_pair("count",  shad::data_types::UINT),
+  std::make_pair("hours",  shad::data_types::UINT)
+};
+static entry_t parse_entry(std::string str,
+                           const shad::data_types::schema_t& schema) {
+  entry_t ret;
+  std::string::iterator start = str.begin();
+  std::string::iterator endit = str.end();
+  for (size_t i = 0; i < n_cols; ++i) {
+    std::string::iterator end = std::find(start, endit, ',');
+    std::string field = std::string(start, end);
+    ret[i] = shad::data_types::encode<uint64_t, std::string>(field, schema[i].second);
+    start = end + 1;
+   }
+   return ret;
+}
+
+
+int main(int argc, char **argv) {
+  std::string filename = "/content/shad/tutorial/Extra02_SHAD_Extensions/tinyfile.csv";
+  std::string line;
+  std::ifstream file(filename);
+  std::vector<std::string> records;
+  if(!file.is_open()) {
+    exit(-1);
+  }
+  while(getline(file, line)) {
+    if(line[0] != '#') {
+      records.push_back(line);
+    }
+  }
+  file.close();
+
+  // Let's create a graph with edges between domains and ips
+  auto eiGraph = shad::EdgeIndex<uint64_t, uint64_t>::Create(n_records);
+  rt::Handle handle;
+  for (auto row : records) {
+    entry_t encoded = parse_entry(row, entry_schema);
+    eiGraph->AsyncInsert(handle, encoded[0], encoded[1]);
+  }
+  rt::waitForCompletion(handle);
+
+  // let's compute the number of unique destination vertices
+  auto setPtr = shad::Set<uint64_t>::Create(n_records);
+  shad::Set<uint64_t>::ObjectID oid = setPtr->GetGlobalID();
+  auto ForEachELambda = [](shad::rt::Handle &h, const uint64_t &src,
+                           const uint64_t &dest, shad::Set<uint64_t>::ObjectID& oid) {
+    auto setPtr = shad::Set<uint64_t>::GetPtr(oid);
+    setPtr->AsyncInsert(h, dest);
+  };
+  eiGraph->AsyncForEachEdge(handle, ForEachELambda, oid);
+  rt::waitForCompletion(handle);
+  size_t size = setPtr->Size();
+  std::cout << "The Graph has " << size << " unique destinations" << std::endl;
+
+  return 0;
+}
+
+}  // namespace shad
